e816ba7fa315d05f9ba7418c08a1eba7bc0c08d5
Amine Hajri <mohamedamine.hajri@gmail.com>
2024-06-23 17:04:00 +0200

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Amine Hajri on 23.06.24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
